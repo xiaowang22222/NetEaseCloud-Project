6b7df17381c47803cb903b773402c7e223d08b5e
2891692713@qq.com <xiaowang2222>
2023-12-13 17:12:35 +0800

    配置项

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
-  // devServer: {
-  //   proxy: 'http://111.229.247.11:3000/'
-  // }
+  configureWebpack:{
+    devtool: "source-map", // Source map generation must be turned on
+    plugins: [
+      sentryWebpackPlugin({
+        org: "neteasecloud-music",
+        project: "vue-sentry",
+        // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+        authToken: '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',
+      }),
+  ],
+  }
 })
