d48165f5a4f92603172fa095e8cb307534216aa0
2891692713@qq.com <xiaowang2222>
2023-12-13 16:07:29 +0800

    监听

--- src/main.js
+++ src/main.js
@@ -14,6 +14,23 @@ import Bus from './Hub'
 import audioList from "@/components/audioList/index.vue"
 import routerfoot from "@/components/routerfoot/index.vue"
 import audioPlay from '@/views/audioPlay/index.js';
+import * as Sentry from "@sentry/vue";
+Sentry.init({
+  Vue,
+  dsn: "https://de0aa59e21b71e309c2e037ec7e43028@o4506386913624064.ingest.sentry.io/4506386975424512",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 Vue.prototype.$bus = Bus
 Vue.use(Howler)
 Vue.use(audioPlay)
@@ -26,6 +43,7 @@ Vue.config.productionTip = false
 Vue.component("Icon",Icon)
 Vue.component("audioList",audioList)
 Vue.component("routerfoot",routerfoot)
+
 new Vue({
   render: h => h(App),
   router,
