b9e447e29546f36a21a617e02aac3b7532c50e6b
2891692713@qq.com <xiaowang2222>
2023-12-17 19:55:25 +0800

    添加雷达歌单功能

--- src/components/home/homepage/Ranking.vue
+++ src/components/home/homepage/Ranking.vue
@@ -4,27 +4,30 @@
             <div class="w-[19vw] scroll-item flex items-center justify-center flex-wrap mr-[6vw] "
                 v-for="item in horizontal.creatives" :key="item.resourceId"> <img :src="item.uiElement.image.imageUrl"
                     alt="">
-                <span
-                    class="currentdate dark:text-[#1a1c23] absolute text-center top-[-1vw] w-[15vw] h-[19vw] leading-[19vw] items-center text-[3.5vw] text-[#fff] font-[800]">{{
-                        item.resourceId == -1 ? currentdate : "" }}</span>
-                <p class="dark:text-[#bcbec5] text-[#666F7D] text-center text-[2.78vw] w-[100%] mt-[2vw]">{{
-                    item.uiElement.mainTitle.title }}</p>
+                <router-link :to="{path:'/playlist',query:{id:item.creativeId}}">
+                    <span
+                        class="currentdate dark:text-[#1a1c23] absolute text-center top-[-1vw] w-[15vw] h-[19vw] leading-[19vw] items-center text-[3.5vw] text-[#fff] font-[800]">{{
+                            item.resourceId == -1 ? currentdate : "" }}</span>
+                    <p class="dark:text-[#bcbec5] text-[#666F7D] text-center text-[2.78vw] w-[100%] mt-[2vw]">{{
+                        item.uiElement.mainTitle.title }}</p>
+                </router-link>
             </div>
         </div>
-
     </Card>
 </template>
 
 <script>
+import { RouterLink } from "vue-router";
 import Card from "./card.vue"
 
 export default {
     components: {
-        Card
+        Card,
+        RouterLink
     },
     props: ['horizontal'],
     created() {
-        console.log(this.horizontal);
+        console.log("adas", this.horizontal);
     }
 }
 </script>
